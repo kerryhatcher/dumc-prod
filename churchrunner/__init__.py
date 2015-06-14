dabaf773b905c518b6e0b2c49867e63e2eb801a4
Kerry Hatcher <kwhatcher@gmail.com>
2015-06-14 16:20:12 -0400

    app config not importing for some reason. disabling for now

--- churchrunner/__init__.py
+++ churchrunner/__init__.py
@@ -2,15 +2,11 @@ __author__ = 'khatcher'
 
 
 from flask import Flask, render_template
-from flask_appconfig import AppConfig
 from flask_bootstrap import Bootstrap
 
 
 def create_app(configfile=None):
     app = Flask(__name__)
-    AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
-                                # highly recommend =)
-                                # https://github.com/mbr/flask-appconfig
     Bootstrap(app)
 
     # in a real app, these should be configured through Flask-Appconfig
