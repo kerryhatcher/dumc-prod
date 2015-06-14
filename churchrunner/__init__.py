83f26e7127b429a2f93daa8531189a09ab71376c
Kerry Hatcher <kwhatcher@gmail.com>
2015-06-14 16:12:45 -0400

    installed bootstrap and started theme work

---
+++ churchrunner/__init__.py
@@ -0,0 +1,32 @@
+__author__ = 'khatcher'
+
+
+from flask import Flask, render_template
+from flask_appconfig import AppConfig
+from flask_bootstrap import Bootstrap
+
+
+def create_app(configfile=None):
+    app = Flask(__name__)
+    AppConfig(app, configfile)  # Flask-Appconfig is not necessary, but
+                                # highly recommend =)
+                                # https://github.com/mbr/flask-appconfig
+    Bootstrap(app)
+
+    # in a real app, these should be configured through Flask-Appconfig
+    app.config['SECRET_KEY'] = 'devkey'
+    app.config['RECAPTCHA_PUBLIC_KEY'] = \
+        '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
+
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    return app
+
+
+
+
+
+
--- dixonumc.py
+++ dixonumc.py
@@ -1,12 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from churchrunner import create_app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run(debug=True)
